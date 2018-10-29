daae7ac500cc3c8c4729539f6395783a54d254c7
ozihler <oliver.zihler@zuehlke.com>
2018-10-29 16:16:52 +0100

    ccws-2018: E11 duplicate solved

--- tests/com/zuehlke/cleancodeworkshop/removeduplications/MathUtilsIsDivisibleBy3Test.java
+++ tests/com/zuehlke/cleancodeworkshop/removeduplications/MathUtilsIsDivisibleBy3Test.java
@@ -13,51 +13,51 @@ public class MathUtilsIsDivisibleBy3Test {
     @Test
     @DisplayName("returns true if the numerator is positive and divisible by 3")
     public void testIsDivisibleBy3_Given_FactorsOf_3() {
-        assertTrue(MathUtils.isDivisibleBy3(3));
-        assertTrue(MathUtils.isDivisibleBy3(6));
-        assertTrue(MathUtils.isDivisibleBy3(9));
-        assertTrue(MathUtils.isDivisibleBy3(15));
-        assertTrue(MathUtils.isDivisibleBy3(30));
+        assertTrue(MathUtils.isDivisibleBy(3, 3));
+        assertTrue(MathUtils.isDivisibleBy(6, 3));
+        assertTrue(MathUtils.isDivisibleBy(9, 3));
+        assertTrue(MathUtils.isDivisibleBy(15, 3));
+        assertTrue(MathUtils.isDivisibleBy(30, 3));
     }
 
     @Test
     @DisplayName("returns true if the numerator is negative and divisible by 3")
     public void testIsDivisibleBy3_Given_Negative_FactorsOf_3() {
-        assertTrue(MathUtils.isDivisibleBy3(-3));
-        assertTrue(MathUtils.isDivisibleBy3(-6));
-        assertTrue(MathUtils.isDivisibleBy3(-9));
-        assertTrue(MathUtils.isDivisibleBy3(-15));
-        assertTrue(MathUtils.isDivisibleBy3(-30));
+        assertTrue(MathUtils.isDivisibleBy(-3, 3));
+        assertTrue(MathUtils.isDivisibleBy(-6, 3));
+        assertTrue(MathUtils.isDivisibleBy(-9, 3));
+        assertTrue(MathUtils.isDivisibleBy(-15, 3));
+        assertTrue(MathUtils.isDivisibleBy(-30, 3));
     }
 
     @Test
     @DisplayName("returns false if the numerator is positive and not divisible by 3")
     public void testIsDivisibleBy3_Given_FailingInputs() {
-        assertFalse(MathUtils.isDivisibleBy3(2));
-        assertFalse(MathUtils.isDivisibleBy3(4));
-        assertFalse(MathUtils.isDivisibleBy3(10));
+        assertFalse(MathUtils.isDivisibleBy(2, 3));
+        assertFalse(MathUtils.isDivisibleBy(4, 3));
+        assertFalse(MathUtils.isDivisibleBy(10, 3));
     }
 
 
     @Test
     @DisplayName("returns false if the numerator is negative but not divisible by 3")
     public void testIsDivisibleBy5_Given_Negative_FailingInputs() {
-        assertFalse(MathUtils.isDivisibleBy3(-2));
-        assertFalse(MathUtils.isDivisibleBy3(-4));
-        assertFalse(MathUtils.isDivisibleBy3(-10));
+        assertFalse(MathUtils.isDivisibleBy(-2, 3));
+        assertFalse(MathUtils.isDivisibleBy(-4, 3));
+        assertFalse(MathUtils.isDivisibleBy(-10, 3));
     }
 
     @Test
     @DisplayName("returns true if the numerator is 0")
     public void testIsDivisibleBy3_Given_0() {
         // 0 is divisible by every number
-        assertTrue(MathUtils.isDivisibleBy3(0));
+        assertTrue(MathUtils.isDivisibleBy(0, 3));
     }
     @Test
     @DisplayName("returns false if the numerator is 1")
     public void testIsDivisibleBy3_Given_1() {
         // 1 isn't divisible by any number except 1 itself
-        assertFalse(MathUtils.isDivisibleBy3(1));
+        assertFalse(MathUtils.isDivisibleBy(1, 3));
     }
 
 
--- tests/com/zuehlke/cleancodeworkshop/removeduplications/MathUtilsIsDivisibleBy5Test.java
+++ tests/com/zuehlke/cleancodeworkshop/removeduplications/MathUtilsIsDivisibleBy5Test.java
@@ -13,47 +13,47 @@ public class MathUtilsIsDivisibleBy5Test {
     @Test
     @DisplayName("returns true if the numerator is positive and divisible by 5")
     public void testIsDivisibleBy5_FactorsOf_5() {
-        assertTrue(MathUtils.isDivisibleBy5(5));
-        assertTrue(MathUtils.isDivisibleBy5(10));
-        assertTrue(MathUtils.isDivisibleBy5(15));
-        assertTrue(MathUtils.isDivisibleBy5(30));
+        assertTrue(MathUtils.isDivisibleBy(5, 5));
+        assertTrue(MathUtils.isDivisibleBy(10, 5));
+        assertTrue(MathUtils.isDivisibleBy(15, 5));
+        assertTrue(MathUtils.isDivisibleBy(30, 5));
     }
 
     @Test
     @DisplayName("returns true if the numerator is negative and divisible by 5")
     public void testIsDivisibleBy5_Given_Negative_FactorsOf_5() {
-        assertTrue(MathUtils.isDivisibleBy5(-5));
-        assertTrue(MathUtils.isDivisibleBy5(-10));
-        assertTrue(MathUtils.isDivisibleBy5(-15));
-        assertTrue(MathUtils.isDivisibleBy5(-30));
+        assertTrue(MathUtils.isDivisibleBy(-5, 5));
+        assertTrue(MathUtils.isDivisibleBy(-10, 5));
+        assertTrue(MathUtils.isDivisibleBy(-15, 5));
+        assertTrue(MathUtils.isDivisibleBy(-30, 5));
     }
 
     @Test
     @DisplayName("returns false if the numerator is positive but not divisible by 5")
     public void testIsDivisibleBy5_Given_FailingInputs() {
-        assertFalse(MathUtils.isDivisibleBy5(4));
-        assertFalse(MathUtils.isDivisibleBy5(6));
+        assertFalse(MathUtils.isDivisibleBy(4, 5));
+        assertFalse(MathUtils.isDivisibleBy(6, 5));
     }
 
 
     @Test
     @DisplayName("returns false if the numerator is negative but not divisible by 5")
     public void testIsDivisibleBy5_Given_Negative_FailingInputs() {
-        assertFalse(MathUtils.isDivisibleBy5(-4));
-        assertFalse(MathUtils.isDivisibleBy5(-6));
+        assertFalse(MathUtils.isDivisibleBy(-4, 5));
+        assertFalse(MathUtils.isDivisibleBy(-6, 5));
     }
 
     @Test
     @DisplayName("returns true if the numerator is 0")
     public void testIsDivisibleBy5_Given_0() {
         // 0 is divisible by every number
-        assertTrue(MathUtils.isDivisibleBy5(0));
+        assertTrue(MathUtils.isDivisibleBy(0, 5));
     }
     @Test
     @DisplayName("returns false if the numerator is 1")
     public void testIsDivisibleBy5_Given_1() {
         // 1 isn't divisible by any number except 1 itself
-        assertFalse(MathUtils.isDivisibleBy5(1));
+        assertFalse(MathUtils.isDivisibleBy(1, 5));
     }
 
 }
