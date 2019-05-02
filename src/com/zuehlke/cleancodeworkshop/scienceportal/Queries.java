f9b9dd01732fe3c6a65ffaa885434a8f018b542b
alinaliburkina <alina.liburkina@gmail.com>
2019-05-02 19:12:35 +0200

    Add science portal

---
+++ src/com/zuehlke/cleancodeworkshop/scienceportal/Queries.java
@@ -0,0 +1,14 @@
+package com.zuehlke.cleancodeworkshop.scienceportal;
+class Queries {
+    static CountSubmissionsQuery countSubmissions(Submissions submissions) {
+        return new CountSubmissionsQuery(submissions);
+    }
+
+    static CollectSubmissionsForAuthorQuery collectSubmissions(Submissions submissions) {
+        return new CollectSubmissionsForAuthorQuery(submissions);
+    }
+
+    static CollectTitlesQuery collectTitles() {
+        return new CollectTitlesQuery();
+    }
+}
